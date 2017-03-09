34b886c3680259402e3ff2fe9c260acbe6a34c70
Matthew Bendall <bendall@gwu.edu>
2017-03-09 15:59:57 -0500

    removed toplevel code from exonicFilter and incorporated into driver

--- mgpcutils/exonicFilter.py
+++ mgpcutils/exonicFilter.py
@@ -1,34 +1,19 @@
 #!/bin/env python2.7
-import os
+# import os
 import csv
 import sys
-from operator import itemgetter
-import operator
+# from operator import itemgetter
+# import operator
 
-from os.path import join, dirname, realpath, split
+# from os.path import join, dirname, realpath, split
+'''
 try:
     scriptdir = dirname(realpath(__file__))
 except NameError:
     scriptdir = dirname(realpath(sys.argv[0]))
 sys.path.append(join(scriptdir, '..', '..', 'common', 'src'))
+'''
 
-from version import VERSION
-VERSION = '1.0.4 (%s)' % (VERSION,)
-
-from optparse_gui import OptionParser
-parser = OptionParser(version=VERSION)
-
-parser.add_option("--exons", type="file", dest="exons", default=None,
-                  help="Exonic coordinates (sored). Required.", notNone=True,
-                  filetypes=[("Exonic Coordinates", "*.txt")])
-parser.add_option("--input", type="file", dest="input", default=None,
-                  help="Input SNVs. Required",
-                  filetypes=[("Input SNV File", "*.vcf;*.csv;*.tsv;*.xls;*.xlsx;*.txt")])
-parser.add_option("--output", type="savefile", dest="output", default=None,
-                  help="Output file. Required",
-                  filetypes=[("Output SNV File", "*.vcf;*.tsv")])
-
-opt, args = parser.parse_args()
 
 from dataset import XLSFileTable, CSVFileTable, TSVFileTable, XLSXFileTable, TXTFileTable
 
@@ -81,117 +66,151 @@ def ReadTSV(filename):
         snvdata.append(ri)
 
     return ["\t".join(snvs.headers())], chrom, snvdata
-												
-exoncoords = opt.exons
-filename = opt.input
-outfile = opt.output
 
-if outfile.endswith('.vcf'):
-    assert(filename.endswith('.vcf'))
-if not filename.endswith('.vcf'):
-    assert(outfile.endswith('.tsv'))
+def main(opt):
+    print opt
+    exoncoords = opt.exons
+    filename = opt.input
+    outfile = opt.output
 
-print "Reading", filename, "...",
-sys.stdout.flush()
+    if outfile.endswith('.vcf'):
+        assert (filename.endswith('.vcf'))
+    if not filename.endswith('.vcf'):
+        assert (outfile.endswith('.tsv'))
 
-if filename.rsplit('.',1)[-1].lower() == 'vcf':
-    fileheader,chrlab,snvdata = ReadVCF(filename)
-else:
-    fileheader,chrlab,snvdata = ReadTSV(filename)
+    print "Reading", filename, "...",
+    sys.stdout.flush()
 
-from chromreg import ChromLabelRegistry
-chrreg = ChromLabelRegistry()
-chrreg.add_labels(filename,chrlab)
+    if filename.rsplit('.', 1)[-1].lower() == 'vcf':
+        fileheader, chrlab, snvdata = ReadVCF(filename)
+    else:
+        fileheader, chrlab, snvdata = ReadTSV(filename)
 
-for i in range(len(snvdata)-1,-1,-1):
-    chrlab = snvdata[i][0]
-    chrom = chrreg.label2chrom(filename,chrlab)
-    if not chrreg.isnumberedchrom(chrom) and \
-       not chrreg.issexchrom(chrom):
-        del snvdata[i]
-        continue
-    snvdata[i][0] = chrom
-    snvdata[i][1] = int(snvdata[i][1])
+    from chromreg import ChromLabelRegistry
+    chrreg = ChromLabelRegistry()
+    chrreg.add_labels(filename, chrlab)
 
-# Expected chromosome labels from UCSC file...
-# Extra numeric chromosomes might make this more robust for organisms
-# with more chromosomes? Doesn't hurt normal (human) case either way.
-# Should we add mitochondria here too? What notation does UCSC use?
-exonlabels = map(str,range(1,100)) + ["X","Y","MT"]
-chrreg.add_labels(exoncoords,exonlabels)
+    for i in range(len(snvdata) - 1, -1, -1):
+        chrlab = snvdata[i][0]
+        chrom = chrreg.label2chrom(filename, chrlab)
+        if not chrreg.isnumberedchrom(chrom) and \
+                not chrreg.issexchrom(chrom):
+            del snvdata[i]
+            continue
+        snvdata[i][0] = chrom
+        snvdata[i][1] = int(snvdata[i][1])
 
-chrreg.default_chrom_order()
-chrorder = chrreg.chrom_order
+    # Expected chromosome labels from UCSC file...
+    # Extra numeric chromosomes might make this more robust for organisms
+    # with more chromosomes? Doesn't hurt normal (human) case either way.
+    # Should we add mitochondria here too? What notation does UCSC use?
+    exonlabels = map(str, range(1, 100)) + ["X", "Y", "MT"]
+    chrreg.add_labels(exoncoords, exonlabels)
 
-snvdata.sort(key=lambda sd: (chrorder(sd[0]),sd[1]))
+    chrreg.default_chrom_order()
+    chrorder = chrreg.chrom_order
 
-print "done"
+    snvdata.sort(key=lambda sd: (chrorder(sd[0]), sd[1]))
 
-#=========================================================================
-# A function called "all_filteration" which does take one argument;
-# that is, a list of all the variants present within the vcf
-# file. This function does the filteration process on the vcf file
-# based the following aspect: The variants chromosomal positions from
-# vcf file have to be within an exonic regions
-#=========================================================================
+    print "done"
+    print "Filtering", filename, "...",
+    sys.stdout.flush()
 
-def all_filteration(d):
+    # =========================================================================
+    # A function called "all_filteration" which does take one argument;
+    # that is, a list of all the variants present within the vcf
+    # file. This function does the filteration process on the vcf file
+    # based the following aspect: The variants chromosomal positions from
+    # vcf file have to be within an exonic regions
+    # =========================================================================
 
-    #
-    # This code assumes that the exon file is sorted by chromosome and
-    # start and end position. Chromosome sort order (for human) is
-    #     1, 2, ..., 9, 10, 11, ...., 19, 20, 21, 22, X, Y.
-    # Position sort order is as integers.
-    #
+    def all_filteration(d):
+        #
+        # This code assumes that the exon file is sorted by chromosome and
+        # start and end position. Chromosome sort order (for human) is
+        #     1, 2, ..., 9, 10, 11, ...., 19, 20, 21, 22, X, Y.
+        # Position sort order is as integers.
+        #
 
-    # Opening, Reading the Exonic coordinates from EMBL database, and
-    # opening the output file to write to
-    with open(exoncoords, 'r') as csvfile:
-        with open(outfile, 'w') as outpt:
-            outpt.write("\n".join(fileheader) + '\n')
+        # Opening, Reading the Exonic coordinates from EMBL database, and
+        # opening the output file to write to
+        with open(exoncoords, 'r') as csvfile:
+            with open(outfile, 'w') as outpt:
+                outpt.write("\n".join(fileheader) + '\n')
 
-            variant_count = 0
-	    last_exonic_coords = (-1e+20,-1e+20,-1e+20)
-            reader = csv.reader(csvfile, delimiter='\t')
-            
-            for row in reader:   # This loop belongs to the exonic coordinates file
+                variant_count = 0
+                last_exonic_coords = (-1e+20, -1e+20, -1e+20)
+                reader = csv.reader(csvfile, delimiter='\t')
 
-                chrom_exonic_coord = chrreg.label2chrom(exoncoords,row[0])
-                assert chrom_exonic_coord != None, "Unexpected chromosome label in exon coordinates file"
-                exonic_start_pos = int(row[1]) + 1
-                exonic_end_pos = int(row[2])
+                for row in reader:  # This loop belongs to the exonic coordinates file
 
-		assert (chrorder(chrom_exonic_coord),exonic_start_pos,exonic_end_pos) >= last_exonic_coords, \
-			"Exon coordinates file is not correctly ordered by chromosome and start/end positions"
+                    chrom_exonic_coord = chrreg.label2chrom(exoncoords, row[0])
+                    assert chrom_exonic_coord != None, "Unexpected chromosome label in exon coordinates file"
+                    exonic_start_pos = int(row[1]) + 1
+                    exonic_end_pos = int(row[2])
 
-                if (variant_count < len(d)):
-                    
-                    chrom_variant = d[variant_count][0]
-                    variant_pos = d[variant_count][1]
-                    
-                    while ((chrom_exonic_coord == chrom_variant) and (exonic_end_pos > variant_pos)) or (chrorder(chrom_exonic_coord) > chrorder(chrom_variant)):
-	
-                        if (chrom_exonic_coord == chrom_variant):
+                    assert (chrorder(chrom_exonic_coord), exonic_start_pos,
+                            exonic_end_pos) >= last_exonic_coords, \
+                        "Exon coordinates file is not correctly ordered by chromosome and start/end positions"
 
-                            if (exonic_start_pos <= variant_pos <= exonic_end_pos):
-
-				chrlab = chrreg.chrom2label(filename,d[variant_count][0])
-				chrpos = str(d[variant_count][1])
-                                outpt.writelines("\t".join([chrlab,chrpos] + d[variant_count][2:]) + '\n')
-
-                        variant_count += 1
-
-                        if (variant_count >= len(d)):
-                            break
+                    if (variant_count < len(d)):
 
                         chrom_variant = d[variant_count][0]
                         variant_pos = d[variant_count][1]
 
-                last_exonic_coords = (chrorder(chrom_exonic_coord),exonic_start_pos,exonic_end_pos)
+                        while ((chrom_exonic_coord == chrom_variant) and (
+                                    exonic_end_pos > variant_pos)) or (
+                                    chrorder(chrom_exonic_coord) > chrorder(
+                                    chrom_variant)):
 
-print "Filtering", filename, "...",
-sys.stdout.flush()
+                            if (chrom_exonic_coord == chrom_variant):
 
-all_filteration(snvdata)
+                                if (
+                                                exonic_start_pos <= variant_pos <= exonic_end_pos):
+                                    chrlab = chrreg.chrom2label(filename,
+                                                                d[
+                                                                    variant_count][
+                                                                    0])
+                                    chrpos = str(d[variant_count][1])
+                                    outpt.writelines("\t".join(
+                                        [chrlab, chrpos] + d[variant_count][
+                                                           2:]) + '\n')
 
-print "done"
+                            variant_count += 1
+
+                            if (variant_count >= len(d)):
+                                break
+
+                            chrom_variant = d[variant_count][0]
+                            variant_pos = d[variant_count][1]
+
+                    last_exonic_coords = (
+                        chrorder(chrom_exonic_coord), exonic_start_pos,
+                        exonic_end_pos)
+
+    all_filteration(snvdata)
+    print "done"
+
+if __name__ == '__main__':
+    from rna2dnalign.version import VERSION
+
+    VERSION = '1.0.4 (%s)' % (VERSION,)
+
+    from mgpcutils.optparse_gui import OptionParser
+
+    parser = OptionParser(version=VERSION)
+
+    parser.add_option("--exons", type="file", dest="exons", default=None,
+                      help="Exonic coordinates (sored). Required.",
+                      notNone=True,
+                      filetypes=[("Exonic Coordinates", "*.txt")])
+    parser.add_option("--input", type="file", dest="input", default=None,
+                      help="Input SNVs. Required",
+                      filetypes=[("Input SNV File",
+                                  "*.vcf;*.csv;*.tsv;*.xls;*.xlsx;*.txt")])
+    parser.add_option("--output", type="savefile", dest="output", default=None,
+                      help="Output file. Required",
+                      filetypes=[("Output SNV File", "*.vcf;*.tsv")])
+
+    opt, args = parser.parse_args()
+    main(opt)
--- rna2dnalign/main.py
+++ rna2dnalign/main.py
@@ -4,12 +4,16 @@
 """
 
 import sys
+import os
+import tempfile
+from collections import namedtuple
 
 # third party imports
 
 # local application imports
 from version import VERSION
 from mgpcutils import optparse_gui
+from mgpcutils import exonicFilter
 # import readCounts
 
 __author__ = 'Matthew L. Bendall'
@@ -22,6 +26,12 @@ try:
 except ImportError:
     print >> sys.stderr, "WX was not imported"
     HAS_WX = False
+
+class OptValues:
+    def __init__(self, **kwargs):
+        self.__dict__ = kwargs
+    def __str__(self):
+        return str(self.__dict__)
 
 if __name__=='__main__':
     if len(sys.argv) == 1 and HAS_WX:
@@ -52,6 +62,37 @@ if __name__=='__main__':
                       remember=True,
                       help="Output folder. Required.", default=None, notNone=True,
                       name="Output Folder")
+
+    ''' Exon filtering group '''
+    exfilt = optparse_gui.OptionGroup(parser, "Filtering")
+    exfilt.add_option("-e", "--exoncoords", type="file", dest="exoncoords",
+                      default=None,
+                      help="Exon coordinates for SNV filtering. Optional.",
+                      name="Exon Coords.",
+                      remember=True,
+                      filetypes=[("Exonic Coordinates", "*.txt")])
+    parser.add_option_group(exfilt)
+
+    ''' Regex group '''
+    regexs = optparse_gui.OptionGroup(parser, "Filename Matching")
+    regexs.add_option("--normaldnare", type="str", dest="normaldnare",
+                      default='GDNA',
+                      help="Germline/Normal DNA filename regular expression. Default: GDNA.",
+                      remember=True, name="Germline DNA")
+    regexs.add_option("--normaltransre", type="str", dest="normaltransre",
+                      default='NRNA',
+                      help="Normal transcriptome filename regular expression. Default: NRNA.",
+                      remember=True, name="Normal Transcr.")
+    regexs.add_option("--tumordnare", type="str", dest="tumordnare",
+                      default='SDNA',
+                      help="Somatic/Tumor DNA filename regular expression. Default: SDNA.",
+                      remember=True, name="Somatic DNA")
+    regexs.add_option("--tumortransre", type="str", dest="tumortransre",
+                      default='TRNA',
+                      help="Tumor transcriptome filename regular expression. Default: TRNA.",
+                      remember=True, name="Tumor Transcr.")
+    parser.add_option_group(regexs)
+
     if gui:
         try:
             opt, args = parser.parse_args(opts=None)
@@ -59,3 +100,79 @@ if __name__=='__main__':
             sys.exit(0)
     else:
         opt, args = parser.parse_args()
+
+    ''' Setup output '''
+    outtemp = './tmpd' # tempfile.mkdtemp()
+
+    ''' Apply exonic filter on SNVs '''
+    if opt.exoncoords:
+        snvfiles = []
+        for snvfile in opt.snvs:
+            dirname, basename = os.path.split(snvfile)
+            prefix, ext = os.path.splitext(basename)
+            outfile = os.path.join(outtemp, '%s.filtered%s' % (prefix, ext))
+            ef_opts = OptValues(
+                exons=opt.exoncoords,
+                input=snvfile,
+                output=outfile
+            )
+            try:
+                exonicFilter.main(ef_opts)
+                snvfiles.append(outfile)
+            except Exception as e:
+                print >>sys.stderr, 'ERROR in exonicFilter, using unfiltered file'
+                print >>sys.stderr, str(e)
+                snvfiles.append(snvfile)
+    else:
+        snvfiles = opt.snvs
+
+    print >> sys.stderr, '\n'.join(snvfiles)
+
+    ''' Apply readCounts to SNVs and aligned reads. '''
+
+
+"""
+python rna2dnalign/main.py -h
+python rna2dnalign/main.py\
+  -s /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.var.flt.vcf\
+  -r /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.sorted.bam\
+  -o .
+
+python rna2dnalign/main.py\
+  -s /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.var.flt.vcf\
+  -r /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.sorted.bam\
+  -e /Volumes/CBI_GRAY/testR2D/UCSC_Human_hg19_RefSeq_CDS_exon_coordinates.txt\
+  -o .
+
+
+python mgpcutils/exonicFilter.py\
+  --exons /Volumes/CBI_GRAY/testR2D/UCSC_Human_hg19_RefSeq_CDS_exon_coordinates.txt\
+  --input /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.var.flt.vcf\
+  --output tmp.vcf
+"""
+
+"""
+from pysam import VariantFile
+vcfin = '/Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.var.flt.vcf'
+exonin = '/Volumes/CBI_GRAY/testR2D/UCSC_Human_hg19_RefSeq_CDS_exon_coordinates.txt'
+out = 'tmp2.txt'
+
+vcf_fh = VariantFile(vcfin)
+vcf_outh = VariantFile(out, 'w', header=vcf_fh.header)
+
+for l in lines:
+    for rec in vcf_fh.fetch(l[0], int(l[1]), int(l[2])):
+        vcf_outh.write(rec)
+
+vcf_outh.close()
+
+bgzip -i /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.var.flt.vcf |\
+bcftools view -R /Volumes/CBI_GRAY/testR2D/UCSC_Human_hg19_RefSeq_CDS_exon_coordinates.txt
+
+bgzip /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.var.flt.vcf
+tabix /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.var.flt.vcf.gz
+bcftools view -R /Volumes/CBI_GRAY/testR2D/UCSC_Human_hg19_RefSeq_CDS_exon_coordinates.txt /Volumes/CBI_GRAY/testR2D/022NTex-TCGA-BH-A0E0-11A-13W-A10F-09_HOLD_QC_PENDING_IlluminaGA-DNASeq_exome.var.flt.vcf.gz
+
+
+
+"""
